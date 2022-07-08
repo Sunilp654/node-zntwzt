ccf1d2a6e564340196a9a6a8fe70bd9a24cc65df
Sunilp654 <sunilkumarp654@gmail.com>
2022-07-08 19:36:44 +0530

    2 files modified

--- src/Comp/Navigation.js
+++ src/Comp/Navigation.js
@@ -7,24 +7,38 @@ function Navigation() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#E">Electronics</Nav.Link>
+            <Nav.Link href="#jewelry">jewelry</Nav.Link>
+            <Nav.Link href="#Men">Men's clothing</Nav.Link>
+            <Nav.Link href="#Women">Women's clothing</Nav.Link>
           </Nav>
+          <NavDropdown
+            title="User Name"
+            id="basic-nav-dropdown"
+            class="float-right"
+          >
+            <NavDropdown.Item href="#action/3.2">Profile</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">
+              WishList{' '}
+              <a href="#login" className="itemQuntity">
+                2
+              </a>
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+          </NavDropdown>
+          <Navbar.Text>
+            Cart
+            <a href="#login" className="itemQuntity">
+              5
+            </a>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
