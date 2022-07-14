c4c07a99810085fb12f303b44206e464e34a5359
Sunilp654 <sunilkumarp654@gmail.com>
2022-07-14 21:28:46 +0530

    1 file modified

--- src/Comp/ProductCard.js
+++ src/Comp/ProductCard.js
@@ -1,37 +1,58 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
 function ProductCard() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const postsData = await response.json();
+      setData(postsData);
+    };
+    fetchdata();
+  }, []);
+
   return (
     <Container className="mt-3 card-section">
       <h3 className="text-center my-5">
         <span className="top-heading">All PRODUCTS</span>
       </h3>
       <Row>
-        <Col md={4} className="mb-2">
-          <Card style={{ width: '100%' }}>
-            <Card.Img
-              variant="top"
-              src="https://expertphotography.b-cdn.net/wp-content/uploads/2018/09/product-photography-types-water-bottle.jpg"
-            />
-            <Card.Body>
-              <Card.Title>Card Title </Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Row className="d-flex flex-row">
-                <Col md={6} xs={6}>
-                  <Button variant="primary">Add To Cart</Button>
-                </Col>
-                <Col md={6} xs={6}>
-                  <Button variant="dark">Add To WishList</Button>
-                </Col>
-              </Row>
-            </Card.Body>
-          </Card>
-        </Col>
+        {data.map((post, index) => {
+          return (
+            <>
+              <Col md={4} className="mb-2" id={post.id}>
+                <Card style={{ width: '100%' }}>
+                  <Card.Img variant="top" src={post.image} />
+                  <Card.Body>
+                    <Card.Title>
+                      {`${post.title.substring(0, 15)}...`}
+                      <span style={{ paddingLeft: '25px', color: 'blue' }}>
+                        {post.price}
+                        {' $'}
+                      </span>
+                    </Card.Title>
+                    <Card.Text>{`${post.description.substring(
+                      0,
+                      50
+                    )}...`}</Card.Text>
+                    <Row className="d-flex flex-row">
+                      <Col md={6} xs={6}>
+                        <Button variant="primary">Add To Cart</Button>
+                      </Col>
+                      <Col md={6} xs={6}>
+                        <Button variant="dark">Add To WishList</Button>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </>
+          );
+        })}
       </Row>
     </Container>
   );
