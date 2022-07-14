d8010b77d9f211af87a1893736cd04b8339b5bdc
Sunilp654 <sunilkumarp654@gmail.com>
2022-07-14 22:59:25 +0530

    2 files modified

--- src/Comp/ProductCard.js
+++ src/Comp/ProductCard.js
@@ -44,7 +44,7 @@ function ProductCard() {
         {data.map((post, index) => {
           return (
             <>
-              <Col md={4} className="mb-2" id={index}>
+              <Col md={4} className="mb-2 producthover" id={index}>
                 <Card style={{ width: '100%' }}>
                   <Card.Img variant="top" src={post.image} />
                   <Card.Body>
