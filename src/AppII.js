3929ee304e24b0ad8836c700bdb217fb4a15f944
Sunilp654 <sunilkumarp654@gmail.com>
2022-08-06 15:14:45 +0530

    2 files modified

--- src/AppII.js
+++ src/AppII.js
@@ -1,17 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
-//import './CompII/Style.css';
-// import NavBar from './CompII/NavBar';
-// import Home from './CompII/Home';
-// import Products from './CompII/Products';
-// import Product from './CompII/Product';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Apidata from './CompII/Apidata';
-import Daysname from './CompII/Daysname';
-import Accordion from 'react-bootstrap/Accordion';
+//import { useState } from 'react';
+import './CompII/Style.css';
+import NavBar from './CompII/NavBar';
+import Home from './CompII/Home';
+import Products from './CompII/Products';
+import Product from './CompII/Product';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Apidata from './CompII/Apidata';
+// import Daysname from './CompII/Daysname';
+// import Accordion from 'react-bootstrap/Accordion';
 
 const AppII = () => {
-  const [data] = useState(Apidata);
+  //const [data] = useState(Apidata);
 
   //const [days] = useState(daysn);
   //const dys = data.applicableOn;
@@ -19,38 +19,30 @@ const AppII = () => {
 
   return (
     <>
-      <Accordion>
+      {/* <Accordion>
         {data.map((dataget) => {
-          //const dys = dataget.applicableOn;
-          //const days = dataget.applicableOn;
-          //const [days] = useState(pieces);
           var daysn = dataget.applicableOn.split(',');
-          //console.log(daysn);
-          //console.log(days);
-          //console.log(pieces);
-          //console.log(dysname);
           return (
             <>
               <Accordion.Item eventKey={dataget.ID}>
                 <Accordion.Header>{dataget.DiscountName}</Accordion.Header>
                 <Accordion.Body>
                   <p>{dataget.StartDate}</p>
-                  {/* <p>var daysn = dataget.applicableOn.split(',');</p> */}
                   <Daysname daysname={daysn} />
                 </Accordion.Body>
               </Accordion.Item>
             </>
           );
         })}
-      </Accordion>
-      {/* <BrowserRouter>
+      </Accordion> */}
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Products" element={<Products />} />
           <Route path="/Product/:id" element={<Product />} />
         </Routes>
-      </BrowserRouter> */}
+      </BrowserRouter>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppII from './AppII';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppII />
   </React.StrictMode>
 );
 
